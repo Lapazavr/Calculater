ab2dd51f7c53ab30bdc21e8250d379923120dc79
Lapazavr <Chmax_12@mail.ru>
2019-02-06 21:41:41 +0300

    Второй коммит. Исправления названий кнопок 7-9. Для кнопок 1-9 в QString собираются либо первый либо второй операнд. В MiniText заносится строка полной истории операций4

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QString> // -//-
+#include <QDebug>  // -//-
+
+QString MiniText = ""; // Малое окно вывода полной последовательности операций
+QString FirstOp = ""; // Первый операнд
+QString SecondOp = ""; // Второй операнд
+bool OperationBool = 0; // Маяк для определения была ли прожата кнопка операции
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +18,212 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_1_clicked()
+{
+    if (OperationBool == 0){
+
+        FirstOp += "1";
+        MiniText += "1";
+
+        qDebug()<<"Кнопка [1] была нажата. Первый операнд"<<endl;
+        qDebug()<<"FirstOp == "<<FirstOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+
+    if (OperationBool == 1){
+
+        SecondOp += "1";
+        MiniText += "1";
+
+        qDebug()<<"Кнопка [1] была нажата. Второй операнд"<<endl;
+        qDebug()<<"SecondOp == "<<SecondOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    if (OperationBool == 0){
+
+        FirstOp += "2";
+        MiniText += "2";
+
+        qDebug()<<"Кнопка [2] была нажата. Первый операнд"<<endl;
+        qDebug()<<"FirstOp == "<<FirstOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+
+    if (OperationBool == 1){
+
+        SecondOp += "2";
+        MiniText += "2";
+
+        qDebug()<<"Кнопка [2] была нажата. Второй операнд"<<endl;
+        qDebug()<<"SecondOp == "<<SecondOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    if (OperationBool == 0){
+
+        FirstOp += "3";
+        MiniText += "3";
+
+        qDebug()<<"Кнопка [3] была нажата. Первый операнд"<<endl;
+        qDebug()<<"FirstOp == "<<FirstOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+
+    if (OperationBool == 1){
+
+        SecondOp += "3";
+        MiniText += "3";
+
+        qDebug()<<"Кнопка [3] была нажата. Второй операнд"<<endl;
+        qDebug()<<"SecondOp == "<<SecondOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    if (OperationBool == 0){
+
+        FirstOp += "4";
+        MiniText += "4";
+
+        qDebug()<<"Кнопка [4] была нажата. Первый операнд"<<endl;
+        qDebug()<<"FirstOp == "<<FirstOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+
+    if (OperationBool == 1){
+
+        SecondOp += "4";
+        MiniText += "4";
+
+        qDebug()<<"Кнопка [4] была нажата. Второй операнд"<<endl;
+        qDebug()<<"SecondOp == "<<SecondOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    if (OperationBool == 0){
+
+        FirstOp += "5";
+        MiniText += "5";
+
+        qDebug()<<"Кнопка [5] была нажата. Первый операнд"<<endl;
+        qDebug()<<"FirstOp == "<<FirstOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+
+    if (OperationBool == 1){
+
+        SecondOp += "5";
+        MiniText += "5";
+
+        qDebug()<<"Кнопка [5] была нажата. Второй операнд"<<endl;
+        qDebug()<<"SecondOp == "<<SecondOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    if (OperationBool == 0){
+
+        FirstOp += "6";
+        MiniText += "6";
+
+        qDebug()<<"Кнопка [6] была нажата. Первый операнд"<<endl;
+        qDebug()<<"FirstOp == "<<FirstOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+
+    if (OperationBool == 1){
+
+        SecondOp += "6";
+        MiniText += "6";
+
+        qDebug()<<"Кнопка [6] была нажата. Второй операнд"<<endl;
+        qDebug()<<"SecondOp == "<<SecondOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+}
+
+void MainWindow::on_pushButton_7_clicked()
+{
+    if (OperationBool == 0){
+
+        FirstOp += "7";
+        MiniText += "7";
+
+        qDebug()<<"Кнопка [7] была нажата. Первый операнд"<<endl;
+        qDebug()<<"FirstOp == "<<FirstOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+
+    if (OperationBool == 1){
+
+        SecondOp += "7";
+        MiniText += "7";
+
+        qDebug()<<"Кнопка [7] была нажата. Второй операнд"<<endl;
+        qDebug()<<"SecondOp == "<<SecondOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+}
+
+void MainWindow::on_pushButton_8_clicked()
+{
+    if (OperationBool == 0){
+
+        FirstOp += "8";
+        MiniText += "8";
+
+        qDebug()<<"Кнопка [8] была нажата. Первый операнд"<<endl;
+        qDebug()<<"FirstOp == "<<FirstOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+
+    if (OperationBool == 1){
+
+        SecondOp += "8";
+        MiniText += "8";
+
+        qDebug()<<"Кнопка [8] была нажата. Второй операнд"<<endl;
+        qDebug()<<"SecondOp == "<<SecondOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+    if (OperationBool == 0){
+
+        FirstOp += "9";
+        MiniText += "9";
+
+        qDebug()<<"Кнопка [9] была нажата. Первый операнд"<<endl;
+        qDebug()<<"FirstOp == "<<FirstOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
+
+    if (OperationBool == 1){
+
+        SecondOp += "9";
+        MiniText += "9";
+
+        qDebug()<<"Кнопка [9] была нажата. Второй операнд"<<endl;
+        qDebug()<<"SecondOp == "<<SecondOp<<endl;
+        qDebug()<<"MiniText == "<<MiniText<<endl;
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,25 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_1_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_9_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
